22e6b6c56bdfc858a92ce68ddc64e003221b8e7e
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-04-01 00:45:03 -0400

    Refactor in order to add some validations on request

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
 				'@typescript-eslint/no-floating-promises': 'warn',
 				'@typescript-eslint/no-misused-promises': 'warn',
 				'@typescript-eslint/no-unsafe-assignment': 'warn',
-				'@typescript-eslint/no-unsafe-argument': 'warn'
+				'@typescript-eslint/no-unsafe-argument': 'warn',
+				'@typescript-eslint/ban-types': 'warn'
 			}
 		}
 	]
